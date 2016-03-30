132a1cfa6639f6d8177e58eec64b1b972376ee87
vtt01uk <vtt01uk@gmail.com>
2016-03-30 17:29:19 +0100

    Able to view snippet of text in index now. Added validations for title and body of text in post.rb file. Modified new and create methods in  posts_controller.rb. Able to display validation error messages when creating new post

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -5,12 +5,16 @@ class PostsController < ApplicationController
   end
   
   def new
+    @post = Post.new
   end
   
   def create
     @post = Post.new(post_params)
-    @post.save
-    redirect_to @post
+    if @post.save
+      redirect_to @post
+    else
+      render 'new'
+    end 
   end
  
   def show
@@ -22,5 +26,4 @@ class PostsController < ApplicationController
     def post_params
       params.require(:post).permit(:title, :body)
     end
-  
 end
