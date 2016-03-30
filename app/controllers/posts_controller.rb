fd8fcc5bd5781465a6bbeed5ce2cef5bcc9b93eb
vtt01uk <vtt01uk@gmail.com>
2016-03-30 14:00:01 +0100

    Generated posts controller. Defined index, new, create and show methods. Modified routes.rb file. Added index, new and show.html.erb files. Created Post model

---
+++ app/controllers/posts_controller.rb
@@ -0,0 +1,25 @@
+class PostsController < ApplicationController
+  
+  def index
+  end
+  
+  def new
+  end
+  
+  def create
+    @post = Post.new(post_params)
+    @post.save
+    redirect_to @post
+  end
+ 
+  def show
+    @post = Post.find(params[:id])
+  end
+  
+  #methods that apply to the methods above in this controller and not no where else
+  private
+    def post_params
+      params.require(:post).permit(:title, :body)
+    end
+  
+end
